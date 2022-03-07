0e8f386e4ae28600aa8d3559a691970a31921f6d
hojoko <hojoko03@gmail.com>
2022-03-07 23:13:16 +0900

    【Add】kaminariとuser/destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_path, alert: "User deleted"
+    else
+      redirect_to users_path, alert: "Failed to delete user"
+    end
   end
 
 end
